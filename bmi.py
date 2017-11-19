085460de6cb0b8d7ac147ed8a3bf4041bffc2578
BartoszStawowy <bartosz.stawowy77@gmail.com>
2017-11-19 21:10:06 +0100

    Update bmi.py

--- bmi.py
+++ bmi.py
@@ -1,34 +1,37 @@
+# -*- coding: utf-8 -*-
+
 from tkinter import *
 from tkinter.messagebox import *
 import tkinter.messagebox
 
 
-okno = Tk()
-okno.title('Licznik BMI')     #tytuł okienka
 
-heading = Label(okno, text = 'Oblicz swoje BMI', font = 'bold')
+window = Tk()
+window.title('Licznik BMI')     #title
+
+heading = Label(window, text = 'Oblicz swoje BMI', font = 'bold')
 heading.grid(row = 0, columnspan = 4)
 
-#wprowadzenie zmiennych
+#inserting variables
 
 cm = IntVar()
-wzrost = Entry(okno, textvariable = cm)
+growth = Entry(window, textvariable = cm)
 kg = IntVar()
-waga = Entry(okno, textvariable = kg)
+weight = Entry(window, textvariable = kg)
 
-wzrost1 = Label(okno, text = 'Wzrost w cm: ')
-waga1 = Label(okno, text = 'Waga w kg: ')
+growth1 = Label(window, text = 'Wzrost w cm: ')
+weight1 = Label(window, text = 'Waga w kg: ')
 
-#funkcje
+#function
 
 def bmi():
    while True:
        try:
-           c = cm.get()
-           c1 = c/100
-           k = kg.get()
-           if k != 0:
-               Ans = (k / (c1 * c1))
+           centimeter = cm.get()
+           c1 = centimeter/100
+           kilograms = kg.get()
+           if kilograms != 0:
+               Ans = (kilograms / (c1 * c1))
                tkinter.messagebox._show('BMI', round(Ans, 2))
                break
            else:
@@ -40,12 +43,12 @@ def bmi():
        except tkinter.TclError:
            tkinter.messagebox._show('Wartość', 'Wpisz wartość!')
            return
-def woda():
+def water():
    while True:
        try:
-           k = kg.get()
-           if k != 0:
-               w = ((k/10)*0.3)
+           kilograms = kg.get()
+           if kilograms != 0:
+               w = ((kilograms/10)*0.3)
                tkinter.messagebox._show('Woda', w)
                break
            else:
@@ -56,18 +59,18 @@ def woda():
            return
 
 
-przycisk1 = Button(okno, text = 'Przelicz BMI', fg = 'black',padx = 30, pady = 17, command = bmi)
-przycisk2 = Button(okno, text = 'Ile litrów wody dziennie\n powinieneś wypić', fg = 'black',padx = 30, pady = 10, command = woda)
+button1 = Button(window, text = 'Przelicz BMI', fg = 'black', command = bmi)
+button2 = Button(window, text = 'Ile litrów wody dziennie\n powinieneś wypić', fg = 'black',command = water)
 
 
-#modelowanie GUI
+#GUI
 
-wzrost1.grid(row = 2, column = 1)
-waga1.grid(row = 3, column = 1)#, padx = 80, pady = 50)
-wzrost.grid(row = 2, column = 2,padx = 30, pady = 10)#, padx = 100, pady = 70)
-waga.grid(row = 3, column = 2,padx = 30, pady = 10)#, padx = 120, pady = 90)
-przycisk1.grid(row = 4, column = 1,padx = 30, pady = 10)#, padx = 40, pady = 10)
-przycisk2.grid(row = 4, column = 2,padx = 30, pady = 10)#, padx = 40, pady = 10)
+growth1.grid(row = 2, column = 1)
+weight1.grid(row = 3, column = 1)
+growth.grid(row = 2, column = 2)
+weight.grid(row = 3, column = 2)
+button1.grid(row = 4, column = 1,padx = 30, pady = 25)
+button2.grid(row = 4, column = 2,padx = 30, pady = 25)
 
 
-okno.mainloop()
+window.mainloop()
