839fc63838d69c4442c6f162ab3c9a05e65039cf
Bartosz Stawowy <bartosz.stawowy77@gmail.com>
2019-06-30 10:56:32 +0200

    Update find_path_in_repo.py

--- find_path_in_repo.py
+++ find_path_in_repo.py
@@ -1,7 +1,11 @@
 import pathlib
 
 def find_path_to_file(file_name):
-   globa_path = pathlib.Path.home()
-   for path in sorted(globa_path.rglob('*')):
-           if str(file_name) in str(path):
-               return str(path)
+    global_path = pathlib.Path.cwd()
+    for path in sorted(global_path.rglob('*')):
+            if str(file_name) in str(path):
+                return str(path)
+            else:
+                for path in sorted(global_path.parent.parent.rglob('*')):
+                    if str(file_name) in str(path):
+                        return str(path)
